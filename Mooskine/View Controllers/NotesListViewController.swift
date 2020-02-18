0dc047dacc79c23a6e6376436e55238e117f187b
Isaac Iniongun <iniongunisaac@gmail.com>
2020-02-18 09:30:58 +0100

    Convert note text to attributed text

--- Mooskine/View Controllers/NotesListViewController.swift
+++ Mooskine/View Controllers/NotesListViewController.swift
@@ -85,7 +85,7 @@ class NotesListViewController: UIViewController, UITableViewDataSource {
     func addNote() {
         
         let note = Note(context: dataController.viewContext)
-        note.text = "New Note"
+        note.attributedText = NSAttributedString(string:  "New Note")
         note.creationDate = Date()
         note.notebook = notebook
         
@@ -130,7 +130,7 @@ class NotesListViewController: UIViewController, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: NoteCell.defaultReuseIdentifier, for: indexPath) as! NoteCell
 
         // Configure cell
-        cell.textPreviewLabel.text = aNote.text
+        cell.textPreviewLabel.attributedText = aNote.attributedText
         if let creationDate = aNote.creationDate {
             cell.dateLabel.text = dateFormatter.string(from: creationDate)
         }
